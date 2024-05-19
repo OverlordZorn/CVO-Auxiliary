02ecfee7d7550adfcb07ac8d785d7762063795f2
Mr. Zorn <dennisAzorn@gmail.com>
2024-05-19 20:46:55 +0200

    new mewsic

--- Addons/music/config.cpp
+++ Addons/music/config.cpp
@@ -34,6 +34,28 @@ class CfgMusicClasses
 	{
 		displayName = "24th Chorni Voron Music";
 	};
+   	class desiderii_Procession
+	{
+		displayName = "Desiderii Marginis - Procession";
+	};
+   	class desiderii_BlackLight
+	{
+		displayName = "Desiderii Marginis - Bathe in Black Light";
+	};
+    class OmnistarEast
+	{
+		displayName = "Omnistar East";
+	};
+    class GameOST
+	{
+		displayName = "Game Soundtracks";
+	};
+    class Others
+	{
+		displayName = "Others";
+	};
+
+
 };
 
 
