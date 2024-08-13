178b03b3945c2aae0e3a23b10b41b2f43a051f46
Mr. Zorn <dennisAzorn@gmail.com>
2024-08-13 18:22:30 +0200

    added QUOTE()

--- addons/adminMessages/gui/sendBoxBase.hpp
+++ addons/adminMessages/gui/sendBoxBase.hpp
@@ -18,10 +18,10 @@ class ga_adminMessages_sendBoxTitleBase: RscTitle {
         "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.8])"
     };
 
-    x = GA_ADMINMESSAGES_TITLE_X;
-    y = GA_ADMINMESSAGES_TITLE_Y;
-    w = GA_ADMINMESSAGES_TOTAL_W;
-    h = GA_ADMINMESSAGES_ELEMENT_H;
+    x = QUOTE(GA_ADMINMESSAGES_TITLE_X);
+    y = QUOTE(GA_ADMINMESSAGES_TITLE_Y);
+    w = QUOTE(GA_ADMINMESSAGES_TOTAL_W);
+    h = QUOTE(GA_ADMINMESSAGES_ELEMENT_H);
 };
 
 class ga_adminMessages_sendBoxBGBase: RscText {
@@ -29,10 +29,10 @@ class ga_adminMessages_sendBoxBGBase: RscText {
 
     colorBackground[] = {0, 0, 0, 0.700000};
 
-    x = GA_ADMINMESSAGES_TITLE_X;
-    y = GA_ADMINMESSAGES_EDITBOX_BG_Y;
-    w = GA_ADMINMESSAGES_TOTAL_W;
-    h = GA_ADMINMESSAGES_EDITBOX_BG_H;
+    x = QUOTE(GA_ADMINMESSAGES_TITLE_X);
+    y = QUOTE(GA_ADMINMESSAGES_EDITBOX_BG_Y);
+    w = QUOTE(GA_ADMINMESSAGES_TOTAL_W);
+    h = QUOTE(GA_ADMINMESSAGES_EDITBOX_BG_H);
 };
 
 class ga_adminMessages_sendBoxBase: RscEdit {
@@ -47,10 +47,10 @@ class ga_adminMessages_sendBoxBase: RscEdit {
     onLoad = "uiNamespace setVariable [""grad_adminMessages_sendBoxCtrl"",(_this select 0)]";
     onUnLoad = "uiNamespace setVariable [""grad_adminMessages_sendBoxCtrl"",nil]";
 
-    x = GA_ADMINMESSAGES_EDITBOX_X;
-    y = GA_ADMINMESSAGES_EDITBOX_Y;
-    w = GA_ADMINMESSAGES_EDITBOX_W;
-    h = GA_ADMINMESSAGES_EDITBOX_H;
+    x = QUOTE(GA_ADMINMESSAGES_EDITBOX_X);
+    y = QUOTE(GA_ADMINMESSAGES_EDITBOX_Y);
+    w = QUOTE(GA_ADMINMESSAGES_EDITBOX_W);
+    h = QUOTE(GA_ADMINMESSAGES_EDITBOX_H);
 };
 
 class ga_adminMessages_sendBoxButtonBase: RscButtonMenu {
@@ -62,10 +62,10 @@ class ga_adminMessages_sendBoxButtonBase: RscButtonMenu {
     text = "$STR_grad_ADMINMESSAGES_SENDBOXBUTTON";
     action = "call grad_adminMessages_fnc_sendMessage";
 
-    x = GA_ADMINMESSAGES_TITLE_X;
-    y = GA_ADMINMESSAGES_SENDBUTTON_Y;
-    w = GA_ADMINMESSAGES_TOTAL_W;
-    h = GA_ADMINMESSAGES_ELEMENT_H;
+    x = QUOTE(GA_ADMINMESSAGES_TITLE_X);
+    y = QUOTE(GA_ADMINMESSAGES_SENDBUTTON_Y);
+    w = QUOTE(GA_ADMINMESSAGES_TOTAL_W);
+    h = QUOTE(GA_ADMINMESSAGES_ELEMENT_H);
 };
 
 class ga_adminMessages_sendBoxListboxBase: RscCombo {
@@ -79,8 +79,8 @@ class ga_adminMessages_sendBoxListboxBase: RscCombo {
     onUnLoad = "uiNamespace setVariable [""grad_adminMessages_sendBoxListboxCtrl"",nil]";
     onLBSelChanged = "_this call grad_adminMessages_fnc_onRecipientChanged";
 
-    x = GA_ADMINMESSAGES_TITLE_X;
-    y = GA_ADMINMESSAGES_SENDBUTTON_Y;
-    w = GA_ADMINMESSAGES_TOTAL_W;
-    h = GA_ADMINMESSAGES_ELEMENT_H;
+    x = QUOTE(GA_ADMINMESSAGES_TITLE_X);
+    y = QUOTE(GA_ADMINMESSAGES_SENDBUTTON_Y);
+    w = QUOTE(GA_ADMINMESSAGES_TOTAL_W);
+    h = QUOTE(GA_ADMINMESSAGES_ELEMENT_H);
 };
