84b6b76155e787df6ec4237d9e981fe29599519d
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-09-20 14:21:16 +0200

    Update config.cpp

--- addons/arsenal/config.cpp
+++ addons/arsenal/config.cpp
@@ -30,34 +30,6 @@ class CfgPatches {
 
 };
 
-class CfgFunctions
-{
-	class CVO_Arsenal            // Tag
-	{
-		class Arsenal           // Category
-		{
-			file = "z\cvo\addons\arsenal\functions";
-
-
-			class eventHandlers  { postInit = 1; };
-			class saveStartingLoadout { postInit = 1; };
-
-			class open	{};
-			class update  {};
-			class addArsenalInteraction {};
-		};
-
-		class CVO_Arsenal_GreenMag
-		{
-			file = "z\cvo\addons\arsenal\functions";
-			requiredAddons[] = { "greenmag_main" };
-
-
-			class greenmag_tab { postInit = 1; };
-		};
-	};
-};
-
 class Extended_PreInit_EventHandlers {
     class CVO_Arsenal_CBA_Settings_XHE_PreInit {
         init = "call compile preprocessFileLineNumbers 'z\cvo\addons\arsenal\XEH_preInit.sqf'";
