346ea418904aaa5626ddaae6c5b14b66c93c440c
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-09-20 15:27:59 +0200

    Create CfgFunctions.hpp

---
+++ addons/bigboom/CfgFunctions.hpp
@@ -0,0 +1,15 @@
+class CfgFunctions
+{
+	class ADDON            // Tag
+	{
+		class COMPONENT           // Category
+		{
+			file = PATH_TO_FUNC;
+
+            class init_cba_eh { postInit = 1; };
+
+    	    class prep {};
+    	    class local_effects {};
+		};
+	}; 
+};
