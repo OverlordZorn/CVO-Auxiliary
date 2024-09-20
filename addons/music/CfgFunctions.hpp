84558e0d7f489a6c51fbb984b486207ef51c999e
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-09-20 14:22:46 +0200

    Update CfgFunctions.hpp

--- addons/music/CfgFunctions.hpp
+++ addons/music/CfgFunctions.hpp
@@ -1,10 +1,19 @@
 class CfgFunctions
 {
+<<<<<<< Updated upstream
 	class ADDON            // Tag
 	{
 		class COMPONENT           // Category
 		{
 			file = PATH_TO_FNC;
+=======
+	class PREFIX            // Tag
+	{
+        tag = ADDON;
+		class COMPONENT           // Category
+		{
+			file = PATH_TO_FUNC;
+>>>>>>> Stashed changes
 			class playlist	    {    postInit = 1;  };
     	    class play          {   };
             class fadeStop      {   };
