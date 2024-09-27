9972c9c42d476015781a6c6de1d3dff7d79d3967
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-09-27 02:03:24 +0200

    Update CfgFunctions.hpp

--- addons/music/CfgFunctions.hpp
+++ addons/music/CfgFunctions.hpp
@@ -5,13 +5,15 @@ class CfgFunctions
 		class COMPONENT           // Category
 		{
 			file = PATH_TO_FUNC;
-            class define_playlists { preInit = 1; };
-			class postInit { postInit = 1; };
-			class playlist { postInit = 1; };
 
             class add_playlist {};
 
+            class define_playlists { preInit = 1; };
+			class postInit { postInit = 1; };
+			
+            class songFromPlaylist {};
     	    class play {};
+
             class fadeStop {};
 		};
 	}; 
