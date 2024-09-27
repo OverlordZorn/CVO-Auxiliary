b6ba524a20b5a13761c789c9e64708f6ad553cf9
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-09-27 02:01:12 +0200

    fix XEH Class

--- addons/intel/XEH_preInit.hpp
+++ addons/intel/XEH_preInit.hpp
@@ -1,5 +1,5 @@
 class Extended_PreInit_EventHandlers {
-    class CVO_Music_CBA_Settings_XHE_PreInit {
+    class CVO_Intel_CBA_Settings_XHE_PreInit {
         init = "call compile preprocessFileLineNumbers 'z\cvo\addons\intel\XEH_preInit.sqf'";
     };
 };
