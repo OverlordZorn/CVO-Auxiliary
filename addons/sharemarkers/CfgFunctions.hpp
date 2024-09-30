d84ca3be9cad787349ae487f9de266961ebfca6b
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-09-30 12:31:35 +0200

    init

---
+++ addons/sharemarkers/CfgFunctions.hpp
@@ -0,0 +1,15 @@
+class CfgFunctions
+{
+    class ADDON
+    {
+        class COMPONENT
+        {
+
+            file = PATH_TO_FUNC;
+
+            class donor {};
+            class reciever {};
+
+        };
+    };
+};
