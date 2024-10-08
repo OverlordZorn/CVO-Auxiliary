1a2f131e701e8188da9b1252dd6f3524d366dfa3
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-10-09 00:43:51 +0200

    introduced by ace

--- addons/temp_rf_compat/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#define COMPONENT Compat_RF
-#define COMPONENT_BEAUTIFIED Temporary Reaction Forces Compatibility Patch
-
-#include "\z\cvo\addons\main\script_mod.hpp"
-#include "\z\cvo\addons\main\script_macros.hpp"
