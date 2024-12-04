fbe67fecf0cddc52501c0da4125160ba5c30bb1a
OverlordZorn <dennisAzorn@gmail.com>
2024-12-04 23:41:07 +0100

    Update script_macros_zrn.hpp

--- addons/main/script_macros_zrn.hpp
+++ addons/main/script_macros_zrn.hpp
@@ -13,6 +13,7 @@
 // Stringtable.xml - use LSTRING() instead
 // #define XML(var1) TRIPLES(STR,ADDON,var1)
 // #define QXML(var1) Q(XML(var1))
+#define SETLSTRING(key) [LSTRING(DOUBLES(set,key)),LSTRING(TRIPLES(set,key,desc))]
 
 // hashMapObjects
 #define OGET(var1) (_self get Q(var1))
